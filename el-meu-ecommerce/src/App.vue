73043064ec55d8f422e9a46aff30c730d876914b
Oriol Tinoco <otinoco@digiocio.com>
2025-02-28 10:49:53 +0100

    Codi 2 - Personalitzant i afegint estils

--- el-meu-ecommerce/src/App.vue
+++ el-meu-ecommerce/src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>Hola món</h1>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<!--Definim la configuració de Vue amb un setup-->
+<script lang="ts" setup>
+console.log("Executat");
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1 {
+  color: red;
 }
 </style>
