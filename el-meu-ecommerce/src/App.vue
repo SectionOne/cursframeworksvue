c00bf8734689e1c554cf0dea0cd72af5d84c3d9c
Oriol Tinoco <otinoco@digiocio.com>
2025-02-28 12:01:47 +0100

    Codi 12 - Pagines que necessitarem per el nostre projecte i Tailwind

--- el-meu-ecommerce/src/App.vue
+++ el-meu-ecommerce/src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <h1  class="text-center text-success display-3">La meva primera app</h1>
-  <hr>
-  <h2>Van entrar</h2><MeuContador :value="5"/>
-  <h2>Van sortir</h2><MeuContador :value="10"/>
+  <PrincipalPage />
 </template>
-
-<!--Definim la configuració de Vue amb un setup-->
 <script lang="ts" setup>
-  import MeuContador from './components/meu-contador/MeuContador.vue';
+import PrincipalPage from './moduls/landing/pagines/PrincipalPage.vue';
 </script>
+<!--No oblidem d'executar npm i tailwind-->
